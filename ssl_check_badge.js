faa4b7e89f1e6b79bc693318693b56d4482027b7
dmd2222 <dmd20@gmx.de>
2023-10-12 22:28:18 +0200

    Add files via upload

---
+++ ssl_check_badge.js
@@ -0,0 +1,21 @@
+<script>
+  // Überprüft, ob die Seite über HTTPS geladen wurde
+  if (window.location.protocol === 'https:') {
+    // Erstellen eines Elements für das Schlosssymbol
+    var lockIcon = document.createElement('div');
+    lockIcon.innerHTML = '🔒 SSL-Verschlüsselt';
+
+    // Stilisieren des Symbols
+    lockIcon.style.position = 'fixed';
+    lockIcon.style.bottom = '10px';
+    lockIcon.style.right = '10px';
+    lockIcon.style.backgroundColor = 'green';
+    lockIcon.style.color = 'white';
+    lockIcon.style.padding = '5px';
+    lockIcon.style.borderRadius = '3px';
+    lockIcon.style.zIndex = '9999';
+
+    // Fügt das Symbol zur Webseite hinzu
+    document.body.appendChild(lockIcon);
+  }
+</script>
